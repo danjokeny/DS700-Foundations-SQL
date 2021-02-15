d3476387fef7d35bb27a017a79a9640e5445bc98
danjokeny <36682157+danjokeny@users.noreply.github.com>
2021-02-15 16:50:11 -0600

    finished DDL DML and control flow

--- ddl dml and control flow.sql
+++ ddl dml and control flow.sql
@@ -33,3 +33,48 @@ from Sales.SalesOrderHeader ;
 
 
 /*DDL*/
+create table person.Superhero
+(heroID int, 
+heroName varchar(50), 
+secretIdentity varchar(50),
+Universe varchar(25), 
+city  varchar(50)
+);
+
+Select * from Person.Superhero
+
+insert into Person.Superhero(heroID, heroName, secretIdentity, Universe, city)
+Values(1, 'Superman', 'clark kent', 'DC', 'Metropolis');
+insert into Person.Superhero(heroID, heroName, secretIdentity, Universe, city)
+Values(2, 'Batman', 'bruce wayne', 'DC', 'Gotham');
+insert into Person.Superhero(heroID, heroName, secretIdentity, Universe, city)
+Values(3, 'Hulk', 'bruce banner', 'Marvel', 'NYC');
+insert into Person.Superhero(heroID, heroName, secretIdentity, Universe, city)
+Values(4, 'Captain America', 'Steve Rogers', 'Marvel', 'Brooklyn');
+insert into Person.Superhero(heroID, heroName, secretIdentity, Universe, city)
+Values(5, 'Godzilla', 'go jira', 'MonsterVerse', 'Tokyo');
+
+/*update statement */
+update Person.Superhero
+set heroName = 'Superman'
+where heroID = 1;
+
+alter table Person.Superhero
+alter column cityLoc varchar(100);
+
+if (DATENAME(M, getdate()) = 'February')
+begin
+	select 'Happy birthday Dawn' as results
+end
+else
+begin
+	select 'not your birthday Dawn' as results
+end
+
+Select s.heroName, s.secretIdentity, s.Universe, s.city,
+	case
+		when universe = 'DC' then 'DC TV Rocks!'
+		when universe = 'Marvel' then 'Marvel movies are better'
+		else 'what universe are you from?'
+	end as UniverseInterpreted
+from Person.Superhero as s;
